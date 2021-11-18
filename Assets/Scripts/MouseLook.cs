99a5a26bacb6991f5a2750295f6797d681ee6f6e
nelson-dylan <36965721+taszty@users.noreply.github.com>
2021-11-17 22:52:52 -0500

    Changelog 1.0
    Added a nighttime skybox
    -Added 2 windows in house
    -Changed global lighting and direction
    -Changed all menu fonts and added dropshadow behind lettering
    -Modified dimension of house to increase size of playable space
    -Added a rain particle effect
    - Added a lamp (not fully functioning yet)

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     // Declare Variables
-    public float mouseSensitivity = 150f;
+    public static float mouseSensitivity = 200f;
 
     public Transform playerBody;
 
@@ -28,5 +28,11 @@ public class MouseLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -70f, 70f);
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    public void SetSensitivity(float SensitivityIndex)
+    {
+        // Changes sensitivity based on slider
+        mouseSensitivity = SensitivityIndex;
     }
 }
